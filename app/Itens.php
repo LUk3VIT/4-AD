3d6463f2f28ed4a84f6d8463d01125021080b3c2
Lucas dias <lucasdiass061@gmail.com>
2022-07-20 13:22:11 -0300

    so falta uns detalhes da pagina de itens, porem a parte principal ja esta concluida

---
+++ app/Itens.php
@@ -0,0 +1,70 @@
+<?php
+
+$itens = [
+    [
+      'itens' => 'bandagem',
+      'valor' => '5'
+    ],
+    [
+      'itens' => 'Feitiço de benção',
+      'valor' => '100'
+    ],
+    [
+      'itens' => 'Frasco de Água benta',
+      'valor' => '30'
+    ],
+    [
+      'itens' => 'Lanterna',
+      'valor' => '4'
+    ],
+    [
+      'itens' => 'Poção de cura',
+      'valor' => '100'
+    ],
+    [
+      'itens' => 'Ritual de ressurreição',
+      'valor' => '1000'
+    ],
+    [
+      'itens' => 'Corda',
+      'valor' => '4'
+    ],
+    [
+      'itens' => 'Escudo',
+      'valor' => '5'
+    ],
+    [
+      'itens' => 'Funda',
+      'valor' => '4'
+    ],
+    [
+      'itens' => 'Arco ',
+      'valor' => '15'
+    ],
+    [
+      'itens' => '12 flechas',
+      'valor' => '6'
+    ],
+    [
+      'itens' => 'Arma de uma mão leve',
+      'valor' => '5'
+    ],
+    [
+      'itens' => 'Arma de uma mão',
+      'valor' => '6'
+    ],
+    [
+      'itens' => 'Arma de duas mãos',
+      'valor' => '15'
+    ],
+    [
+      'itens' => 'Armadura leve',
+      'valor' => '10'
+    ],
+    [
+      'itens' => 'Armadura pesada',
+      'valor' => '30'
+    ],
+  ];
+
+?>
