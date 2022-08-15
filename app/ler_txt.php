370353e36a7721762bb16beead9cf211837f6b94
LUk3VIT <lucasdiass061@gamil.com>
2022-08-15 13:23:40 -0300

    atualização front 1.1 back 1.5

--- app/ler_txt.php
+++ app/ler_txt.php
@@ -1,14 +1,58 @@
 <?php
 
+    session_start();
     $arquivo = fopen("../tabelas/tabela_minions.txt","r");
 
     while (!feof($arquivo)) {
         $linhas = fgets($arquivo);
-        $dados = explode(",",$linhas);
+        $dados = explode(";",$linhas);
         
-        $campo1 = $dados[0];
-        echo ($campo1);
-        
+        $_SESSION['campo0'] = $dados[0];
+        $_SESSION['campo1'] = $dados[1];
+        $_SESSION['campo2'] = $dados[2];
+        $_SESSION['campo3'] = $dados[3];
+        $_SESSION['campo4'] = $dados[4];
+        $_SESSION['campo5'] = $dados[5];
+        $_SESSION['campo6'] = $dados[6];
+        $_SESSION['campo7'] = $dados[7];
+        $_SESSION['campo8'] = $dados[8];
+        $_SESSION['campo9'] = $dados[9];
+        $_SESSION['campo10'] = $dados[10];
+        $_SESSION['campo11'] = $dados[11];
+        $_SESSION['campo12'] = $dados[12];
+        $_SESSION['campo13'] = $dados[13];
+        $_SESSION['campo14'] = $dados[14];
+        $_SESSION['campo15'] = $dados[15];
+        $_SESSION['campo16'] = $dados[16];
+        $_SESSION['campo17'] = $dados[17];
+        $_SESSION['campo18'] = $dados[18];
+        $_SESSION['campo19'] = $dados[19];
+        $_SESSION['campo20'] = $dados[20];
+        $_SESSION['campo21'] = $dados[21];
+        $_SESSION['campo22'] = $dados[22];
+        $_SESSION['campo23'] = $dados[23];
+        $_SESSION['campo24'] = $dados[24];
+        $_SESSION['campo25'] = $dados[25];
+        $_SESSION['campo26'] = $dados[26];
+        $_SESSION['campo27'] = $dados[27];
+        $_SESSION['campo28'] = $dados[28];
+        $_SESSION['campo29'] = $dados[29];
+        $_SESSION['campo30'] = $dados[30];
+        $_SESSION['campo31'] = $dados[31];
+        $_SESSION['campo32'] = $dados[32];
+        $_SESSION['campo33'] = $dados[33];
+        $_SESSION['campo34'] = $dados[34];
+        $_SESSION['campo35'] = $dados[35];
+        $_SESSION['campo36'] = $dados[36];
+        $_SESSION['campo37'] = $dados[37];
+        $_SESSION['campo38'] = $dados[38];
+        $_SESSION['campo39'] = $dados[39];
+        $_SESSION['campo40'] = $dados[40];
+        $_SESSION['campo41'] = $dados[41];
+        $_SESSION['campo42'] = $dados[42];
+
+
+        echo $_SESSION['campo28'];
     }
 
 ?>
--- nav/tabelas.php
+++ nav/tabelas.php
@@ -72,21 +72,22 @@
           <select class="form-select tabela__select" aria-label="Default select example">
             <option selected>
               <h2 class="tabela__select__h2">Tabela de Minions</h2>
-              <?php
-                $_SESSION['tbl'] = "tbl_minions";
-              ?>
             </option>
           </select>
           <h1 class="tabela__h1">Tabela de Minions D6</h1>
           <div class="tabela__informacao">
             <p class="tabela__informacao__p">
-              1
-              <br>
-              <br>
-              1D6 + 2 esqueletos ou 1d6 zumbis (50% de chance de cada um). 
-              Mortos-vivos nível 3. Nenhum tesouro. Armas esmagadoras atacam
-              Esqueletos em +1. As setas estão em - 1 contra esqueletos e zumbis.
-              Esqueletos e zumbis nunca testam moral. Reações: sempre lutar até a morte.
+              <?php
+                echo"<li>".$_SESSION['campo0']."</li>";
+                echo "<br>";
+                echo"<li>".$_SESSION['campo1']."</li>";
+                echo"<li>".$_SESSION['campo2']."</li>";
+                echo"<li>".$_SESSION['campo3']."</li>";
+                echo"<li>".$_SESSION['campo4']."</li>";
+                echo"<li>".$_SESSION['campo5']."</li>";
+                echo"<li>".$_SESSION['campo6']."</li>";
+                echo"<li>".$_SESSION['campo7']."</li>";
+              ?>
             </p>
             <img class="tabela__informacao__img" src="/assets/img/Minions.png" alt="">
           </div>
